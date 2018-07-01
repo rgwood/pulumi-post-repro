95d5a60f6ddfb0ef1d4da7d4737eb82f27cceb17
Reilly <reilly.wood@icloud.com>
2018-07-01 09:46:38 -0700

    switch to API from deprecated HttpEndpoint

--- index.ts
+++ index.ts
@@ -2,16 +2,20 @@ import * as pulumi from "@pulumi/pulumi";
 import {Request, Response} from "@pulumi/cloud";
 import * as cloud from "@pulumi/cloud-aws";
 
-let endpoint = new cloud.HttpEndpoint("hello-world");
+let endpoint = new cloud.API("hello-world");
 
 // POST calls work but OPTIONS calls with Access-Control-Request-Method=POST fail with a 404 and x-amzn-ErrorType=MissingAuthenticationTokenException
 endpoint.post("/", (req: Request, res: Response) => {
     res.status(200).setHeader('Access-Control-Allow-Origin', '*').json({hello: 'world'});
 });
 
-// Tried manually wiring up the OPTIONS response, but I get the same result
+// If I try manually wiring up the OPTIONS response, then I get a 502 Bad Gateway
 endpoint.options("/", (req: Request, res: Response) => {
-    res.status(200).setHeader('Access-Control-Allow-Origin', '*');
+    try {
+        res.status(200).setHeader('Access-Control-Allow-Origin', '*');        
+    } catch (error) {
+     console.log({error});
+    }
 });
 
 exports.endpoint = endpoint.publish().url;
